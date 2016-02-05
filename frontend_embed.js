1743974d2dcd0529206f59b7ac1a19d5e59979e0
Ilan Granot <ilangranot@Ilans-MacBook-Pro.local>
2016-02-05 14:19:12 -0800

    adding basic html and js for testing

---
+++ frontend_embed.js
@@ -0,0 +1,17 @@
+// Frontend test js example
+// Suppose to be a calculator
+var a = 1;
+
+var b = 2;
+
+var operation = "plus";
+
+function runOp(x,y,operand){
+  var result;
+  if (operand == "plus"){
+    result = a+b;
+  }
+  return result;
+}
+
+document.getElementById("calculator").value = runOp(a,b,operation);
