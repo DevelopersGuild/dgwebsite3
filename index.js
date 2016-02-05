a5bb9ed0b75308376c360b2f105b81d26d1fc408
BlueAccords <myoldemailname@gmail.com>
2016-02-05 14:11:04 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// The main server file.
+// Building a new website for the DG website from scratch.
+
+// Configuration/requirement modules.
+// these files are located in the package.json
+var express = require('express');
+var app = express();
+
+// Route localhost:3000 and send the text 'Page is here' to the client
+app.get('/', function(req, res) {
+  res.send('Page is here');
+});
+
+// Listen on port 3000. and display in the terminal 'app running on port 3000'
+// if its working.
+// app will be running on localhost:3000
+app.listen(3000, function() {
+  console.log(' app running on port 3000');
+});
