49115a7ac1a831ed1ac294f566c3786463634f2b
BlueAccords <myoldemailname@gmail.com>
2016-02-05 15:39:05 -0800

    refactored second html page route and more config settings for view engine/view directory

--- server.js
+++ server.js
@@ -17,19 +17,28 @@ nunjucks.configure('views', {
   watch: true,
 });
 
-// app.set('view engine', 'html');
+// Set the directory for the server to look for view pages.
+// can be an array of directories.
+// app.set('views', [Array of Directory Strings])
+// similar to app.use(express.static(...)) but explictily for view pages.
+app.set('views', 'views');
+
+// setting the view engine sets the default extension to use
+// if an extension is omitted.
+app.set('view engine', 'html');
 
 // Set path for routes to check for static files(html/css/javascript) in
 // the public folder
 app.use(express.static('public'));
-
-app.use(express.static('views'));
 
 // Route localhost:3000 and send the text 'Page is here' to the client
 app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.get('/second', function(req, res) {
+  res.render('second');
+});
 // Listen on port 3000. and display in the terminal 'app running on port 3000'
 // if its working.
 // app will be running on localhost:3000
