b73a94267a9a72054db5f813450c02dbd819914b
Vihan Chaudhry <vihanchaudhry@gmail.com>
2016-04-22 22:41:21 +0000

    Created controller for GitHub repositories

---
+++ server/controllers/repositories.js
@@ -0,0 +1,10 @@
+// Repositories controller for GitHub integration
+'use strict';
+
+var getRepositoryList = function() {
+    // TODO: get list of repositories with URLs
+};
+
+var getRepository = function() {
+    // TODO: get individual repository object
+};
--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,9 @@ var express = require('express');
 
 var router = express.Router();
 
+// Controller for repositories
+var repositories = require('./controllers/repositories');
+
 // Root route
 router.get('/', function(req, res) {
     res.render('second', {
